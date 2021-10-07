a3b8447721b93287c0d41c8c0babfd9c2b20c614
Paul Lemus <paullemus@protonmail.com>
2021-10-06 23:20:46 -0700

    added ugfx, contrib, lufa

--- quantum/version.h
+++ quantum/version.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#define QMK_VERSION "62d9c6-dirty"
-#define QMK_BUILDDATE "2021-10-06-23:17:17"
+#define QMK_VERSION "adc052-dirty"
+#define QMK_BUILDDATE "2021-10-06-23:19:46"
 #define CHIBIOS_VERSION "develop_2021_q3"
-#define CHIBIOS_CONTRIB_VERSION "2021-10-06-23:17:17"
+#define CHIBIOS_CONTRIB_VERSION "develop_2021_q3"
